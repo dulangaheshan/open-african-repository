9d46d0bde484b8299ab7e3d432d0ac96240b79ec
Dulanga Heshan <164056m@uom.lk>
2018-07-07 00:57:28 +0530

    i designed according to templte by using angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { FooterComponent } from './footer/footer.component';
+import { Footer2Component } from './footer2/footer2.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent,
+    SignUpComponent,
+    HomeComponent,
+    FooterComponent,
+    Footer2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatCardModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
